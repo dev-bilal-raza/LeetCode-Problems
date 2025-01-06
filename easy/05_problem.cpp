16eed9240745cbe36110ebe113890304301a6f5e
dev-bilal-raza <success9262@gmail.com>
2025-01-06 17:47:39 +0500

    Add implementation of palindrome check with alphanumeric filtering in easy category

---
+++ easy/05_problem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isAlphaNum(char ch)
+    {
+        if ((ch >= '0' && ch < '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z'))
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s)
+    {
+        int st = 0;
+        int end = s.size() - 1;
+        while (st < end)
+        {
+            if (!isAlphaNum(s[st]))
+            {
+                st++;
+                continue;
+            }
+            if (!isAlphaNum(s[end]))
+            {
+                end--;
+                continue;
+            }
+            if (tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    cout << "Is Palindrome: " << Solution().isPalindrome("m0ada&0m") << endl;
+    return 0;
+}
