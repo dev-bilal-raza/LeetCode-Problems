405f9774c4c2e69821176f7fe9e7783aea47bbc9
dev-bilal-raza <success9262@gmail.com>
2025-01-06 16:01:26 +0500

    Add implementation of maximum subarray sum in medium category

--- medium/01_problem.cpp
+++ medium/01_problem.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int subArrSum = 0;
+        for (int num : nums)
+        {
+            subArrSum += num;
+            ans = max(subArrSum, ans);
+            if (subArrSum < 0)
+            {
+                subArrSum = 0;
+            }
+        }
+        return ans;
+    };
+};
 
 int main()
 {
+    vector<int> vec = {1, 2, 3, -8, -3};
+    cout << "Maximum SubArray Sum: " << Solution().maxSubArray(vec) << endl;
     return 0;
 }
