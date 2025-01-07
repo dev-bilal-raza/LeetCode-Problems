b5e25315a36ce2e7607b0ac298c9c9f1e3c320d8
dev-bilal-raza <success9262@gmail.com>
2025-01-08 03:28:38 +0500

    Refactor maximum profit calculation to improve efficiency and readability

--- easy/06_problem.cpp
+++ easy/06_problem.cpp
@@ -7,19 +7,15 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
-        int dayToBuy = 0, dayToSell = 1, max_profit = 0;
-        while (dayToSell < prices.size())
-        {
-            if (prices[dayToBuy] < prices[dayToSell])
-            {
-                int profit = prices[dayToSell] - prices[dayToBuy];
+        int dayToBuy = 0, max_profit = 0;
+        for (int i = 1; i < prices.size(); i++){
+            if (prices[dayToBuy] < prices[i]){
+                int profit = prices[i] - prices[dayToBuy];
                 max_profit = max(profit, max_profit);
             }
-            else
-            {
-                dayToBuy=dayToSell;
+            else{
+                dayToBuy = i;
             }
-            dayToSell++;
         }
         return max_profit;
     }
@@ -27,7 +23,7 @@ public:
 
 int main()
 {
-    vector<int> prices = { 1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9 };
+    vector<int> prices = {1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9};
     cout << "Maximum Profit: " << Solution().maxProfit(prices) << endl;
     return 0;
 }
